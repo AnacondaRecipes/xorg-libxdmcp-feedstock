d9899b5105b1c684de490fa32e11c2668b48bb7c
Serhii Kupriienko <skupriienko@anaconda.com>
2025-04-25 10:52:20 +0300

    Try fix win

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,24 +32,26 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
     )
     autoreconf "${autoreconf_args[@]}"
 
-    # And we need to add the search path that lets libtool find the
-    # msys2 stub libraries for ws2_32.
-    for potential_path in "$BUILD_PREFIX_M/Library/mingw-w64/lib" "$mprefix/mingw-w64/lib"; do
-        if [ -e "$potential_path/libws2_32.a" ]; then
-            platlibs=$(cygpath -w "$potential_path")
-            echo "Found msys2 libs at: $platlibs"
-            export LDFLAGS="$LDFLAGS -L$platlibs"
-            break
+    # Look in standard mingw-w64 library locations
+    for lib_path in "$BUILD_PREFIX_M/Library/mingw-w64/bin" "$BUILD_PREFIX_M/Library/bin" "$mprefix/Library/mingw-w64/lib" "$mprefix/Library/lib"; do
+        if [ -d "$lib_path" ]; then
+            # Convert to Windows path
+            win_path=$(cygpath -w "$lib_path")
+            echo "Adding path to library search: $win_path"
+            export PATH="$PATH:$win_path"
+            export LIBRARY_PATH="$LIBRARY_PATH:$win_path"
+            # For ld
+            export LDFLAGS="$LDFLAGS -L$win_path"
         fi
     done
     
-    if [ -z "$platlibs" ]; then
-        echo "Warning: Could not locate libws2_32.a, using default path"
-        default_path="$BUILD_PREFIX_M/Library/mingw-w64/lib"
-        platlibs=$(cygpath -w "$default_path")
-        export LDFLAGS="$LDFLAGS -L$platlibs"
-    fi
+    # Explicitly add the Windows socket library to LIBS
+    export LIBS="$LIBS -lws2_32"
+    
+    # Skip tests on Windows
+    export do_check=no
 else
+    export do_check=yes
     # for other platforms we just need to reconf to get the correct achitecture
     echo libtoolize
     libtoolize
@@ -77,10 +79,10 @@ configure_args=(
 make -j$CPU_COUNT
 make install
 
-# Disable make check because it fails on win-64
+# Disable make check because it fails on win-64:
 # FAIL: Array.exe
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-#     make check
-# fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" && "${do_check:-yes}" == "yes" ]]; then
+    make check
+fi
 
 rm -rf $uprefix/share/doc/libXdmcp
